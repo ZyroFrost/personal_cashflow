8b1c735f69f2c2b2f8aa28b3ce05274def8a3b12
Zyro <zyrofrost@gmail.com>
2025-12-25 17:29:26 +0700

    fix only show debug on local

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import io, base64, time # chuyển đổi Image to base64 bỏ vào Icon button
+import io, base64, time, os
 from PIL import Image # đọc image từa folder assets
 from streamlit_option_menu import option_menu # thư viện mở rộng của streamlit để add icon với css
 from streamlit_extras.stylable_container import stylable_container
@@ -209,4 +209,9 @@ elif st.session_state['current_page'] == "Log out":
     st.logout()
 
 # Debug
-st.write("DEBUG:", st.secrets.get("auth", {}).get("redirect_uri"))
+is_cloud = os.getenv("STREAMLIT_CLOUD") == "true"
+
+# Debug ONLY on local
+if not is_cloud:
+    redirect_uri = st.secrets.get("auth", {}).get("redirect_uri", "NOT SET")
+    st.write("DEBUG (LOCAL): redirect_uri =", redirect_uri)
